2fa279cfa398239e61e9671d24741f0bbca6aa51
Dell <Dell@DESKTOP-KN5QCR3>
2024-05-02 22:50:37 +0530

    Intial Commit

---
+++ Category-project/src/test/java/com/categorycrud/Category/project/CategoryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.categorycrud.Category.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
